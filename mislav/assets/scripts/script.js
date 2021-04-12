c4093d5b64915f58734c6ad391e3d6408ee56e15
cavisbo <73191528+cavisbo@users.noreply.github.com>
2021-04-13 01:30:41 +0200

    woof

--- mislav/assets/scripts/script.js
+++ mislav/assets/scripts/script.js
@@ -1,26 +1,40 @@
-(()=>{
+import { createAddIngredient, createRemoveIngredient, createCartItem } from "./elements.js";
+
+(() => {
 
     // HTML Buttons
     let btnClear = null;
     let btnAddToCart = null;
-    let btnDeleteCookies = null;
+    let btnDeleteSession = null;
     let btnReloadPage = null;
 
     // HTML Containers
     let contCart = null;
     let contJson = null;
+    let contAddIngredient = null;
+    let contRemoveIngredient = null;
+    let contBurgerPrice = null;
 
     // Data
     let cart = null;
-    let json = null;
+    let burger = null;
+    let burgerPrice = null;
+    let ingredients = null;
 
     // Initialization Function (Setup)
     const init = () => {
-        // loadData();
+        initData();
         initButtons();
         initHandlers();
         initElements();
         updateScreen();
+    }
+
+    const initData = () => {
+        burger = [];
+        ingredients = loadJSON("assets/data/ingredients.json");
+        //drinks = loadJSON("assets/data/drinks.json");
+        //fries = loadJSON("assets/data/fries.json");
     }
 
     const initButtons = () => {
@@ -33,15 +47,19 @@
     const initElements = () => {
         contCart = document.querySelector("#cont-cart");
         contJson = document.querySelector("#cont-json");
+        contAddIngredient = document.querySelector("#cont-add-ingredient");
+        contRemoveIngredient = document.querySelector("#cont-remove-ingredient");
+        contBurgerPrice = document.querySelector("#cont-burger-price");
     }
 
     const initHandlers = () => {
         btnClear.onclick = () => {
-
+            burger = [];
+            updateScreen();
         };
         btnAddToCart.onclick = () => {
             addToCart({
-                ID: cart.length>0?cart[cart.length-1].ID+1:0
+                ID: cart.length > 0 ? cart[cart.length - 1].ID + 1 : 0
             });
             updateScreen();
         };
@@ -49,14 +67,15 @@
             clearSession();
             updateScreen();
         };
-        btnReloadPage.onclick = () => {
-            reloadPage();
-        };
+        btnReloadPage.onclick = () => reloadPage();
     };
 
     const updateScreen = () => {
         updateCart();
         updateJson();
+        updateAddList();
+        updateRemoveList();
+        updateBurgerPrice();
     }
 
     const addToCart = (data) => {
@@ -65,47 +84,95 @@
     };
 
     const updateCart = () => {
-        cart = getSession("cart")?getSession("cart"):[];
+        cart = getSession("cart") ? getSession("cart") : [];
         clearContainer(contCart);
-        for (item of cart) {
-            contCart.appendChild(parseToHTML(
-                `
-                <div class="burger rounded m-1 d-flex justify-content-center align-items-center position-relative">
-                <h4 class="position-absolute text-light">${item.ID}</h4>
-                <img class=" img-fluid" src="/assets/images/burger.png" alt="">
-                </div>
-                `
-            ));
+        for (let item of cart) {
+            contCart.appendChild(createCartItem(item));
         };
     };
 
+    const updateAddList = () => {
+        clearContainer(contAddIngredient);
+        for (let ingredient of ingredients) {
+            let addIng = createAddIngredient(ingredient);
+            addIng.onclick = () => {
+                burger.push(ingredient);
+                let remIng = createRemoveIngredient(ingredient);
+                remIng.onclick = () => {
+                    removeFrom(burger, ingredient);
+                    uupdateScreen();
+                }
+                contRemoveIngredient.appendChild(
+                    remIng
+                );
+                updateScreen();
+            }
+            contAddIngredient.appendChild(
+                addIng
+            );
+        };
+    }
+
+    const removeFrom = (arr, toDelete) => {
+        arr.splice(arr.indexOf(toDelete), 1);
+    }
+
+    const updateRemoveList = () => {
+        clearContainer(contRemoveIngredient);
+        for (let ingredient of burger) {
+            let remIng = createRemoveIngredient(ingredient);
+            remIng.onclick = () => {
+                removeFrom(burger, ingredient);
+                updateScreen();
+            }
+            contRemoveIngredient.appendChild(
+                remIng
+            );
+        }
+    }
+
     const updateJson = () => {
         contJson.innerHTML = prettyJson();
+    }
+
+    const updateBurgerPrice = () => {
+        burgerPrice = 0;
+        for (let ingredient of burger) {
+            burgerPrice += ingredient.price;
+        }
+        contBurgerPrice.innerHTML = `$${burgerPrice}`;
+    }
+
+    const loadJSON = (url) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.send(null);
+        return JSON.parse(xhr.responseText);
     }
 
     const prettyJson = () => {
         let ugly = JSON.stringify(cart, null, ' ');
         let pretty = "";
         let depth = 0;
-        for (word of ugly.split(" ")) {
+        for (let word of ugly.split(" ")) {
             if (word.trim() == "[" || word.trim() == "{") depth++;
             if (word.trim() == "]" || word.trim() == "},") depth--;
             if (["}\n]"].includes(word.trim())) {
                 pretty += `<br/>${"&#8195;".repeat(depth)}}<br/>]`;
             } else {
-                pretty += `${["]", "},"].includes(word.trim())?"<br/>":""}${"&#8195;".repeat(depth)}${word}${["[","{"].includes(word.trim())?"<br/>":""}`;
+                pretty += `${["]", "},"].includes(word.trim()) ? "<br/>" : ""}${"&#8195;".repeat(depth)}${word}${["[", "{"].includes(word.trim()) ? "<br/>" : ""}`;
             }
         }
         return pretty;
     }
 
     const main = () => {
-        
+
     };
 
     // Set and Get data from local storage
     const setSession = (key, value) => sessionStorage[key] = JSON.stringify(value);
-    const getSession = (key) => JSON.parse(sessionStorage[key]?sessionStorage[key]:null);
+    const getSession = (key) => JSON.parse(sessionStorage[key] ? sessionStorage[key] : null);
 
     // Clear session
     const clearSession = () => sessionStorage.clear();
